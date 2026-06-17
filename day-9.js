984d06d7fb0ae89c81e7a1b87be54e23ba5a3869
Fahim2023 <fahimshahrier2023@gmail.com>
2026-06-17 17:04:13 +0600

    solved the problem number 43

--- day-9.js
+++ day-9.js
@@ -44,6 +44,20 @@ console.log(isAnagram("hello", "world"));
 // Input: [1,2,4,5], n=5  → Output: 3
 // Hint: Use the formula: expected sum = n*(n+1)/2, then subtract actual sum.
 
+// Answer Of the Problem 43 :
+function findMissing(arr, n) {
+  const sum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // const element = arr[i ];
+    actualSum += arr[i];
+  }
+  //   for (let number of arr) {
+  //     actualSum += number;
+  //   }
+  return sum - actualSum;
+}
+console.log(findMissing([1, 2, 4, 5], 5));
 // Problem 44: Valid Parentheses  [Medium]
 // Description: Write a function isValidParentheses(str) that returns true if the string has valid, balanced parentheses, brackets, and braces.
 // Example:
