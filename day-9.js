e5db5adc66df5d4353547f80ddc6b9a22e8aba89
Fahim2023 <fahimshahrier2023@gmail.com>
2026-06-20 00:07:47 +0600

    solved problem number 45

--- day-9.js
+++ day-9.js
@@ -114,8 +114,36 @@ function isValidParentheses(str) {
 
 console.log(isValidParentheses("([)]"));
 console.log(isValidParentheses("()[]{}"));
+
 // Problem 45: Binary Search  [Medium]
 // Description: Write a function binarySearch(arr, target) that searches a sorted array and returns the index of the target, or -1 if not found.
 // Example:
 // Input: [1,3,5,7,9], target=7  → Output: 3
 // Hint: Use left and right pointers; check the middle element each iteration.
+
+// Answer Of the Problem number 50 :
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    }
+
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+const result = binarySearch([1, 3, 5, 7, 9], 7);
+
+console.log(result);
